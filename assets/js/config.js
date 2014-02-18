04612b9f9de2833a3ca0393f59e1e2a31a3b4423
Jessica Graham <jgraham@amplify.com>
2014-02-18 18:17:25 -0500

    Show set of cards with symbols

---
+++ assets/js/config.js
@@ -0,0 +1,15 @@
+config = {
+  CARD_SYMBOLS : [
+    'fa-camera',
+    'fa-certificate',
+    'fa-comment',
+    'fa-dashboard',
+    'fa-fighter-jet',
+    'fa-flash',
+    'fa-heart',
+    'fa-star',
+    'fa-wrench',
+    'fa-money',
+    'fa-scissors'
+  ]
+};
